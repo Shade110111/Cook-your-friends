f2d077f81342ba484337097ebb8419685d27eda9
Shade <edmond.rickard@gmail.com>
2026-02-09 11:05:50 +0000

    everything is fixed so long as the image is larger than the screen!!!!!!

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
 let player1 = {x:0,y:0,dx:0,dy:0}
 let player2 = {x:0,y:0,dx:0,dy:0}
-let camera = {x:0,y:0,zoom:0}
-let move_speed = 10 //move speed is absolute units
-let unit //square real screen
-let unit_offset = {x:0,y:0}
-let level_data = {side:1200}
+let camera = {x:0,y:0}
+let move_speed = 5 //move speed is absolute units
+
+
 
 function preload(){
   level = loadImage('Level.png');
@@ -12,22 +11,14 @@ function preload(){
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  unit = (windowWidth+windowHeight)/2 //unit is the average of the height and width of the screen, use it for all scaling and co-ordinates.
-  unit_offset.x = (windowWidth-unit)/2
-  unit_offset.y = (windowHeight-unit)/2
-  player1 = {x:unit/2,y:+unit/2}
-  player2 = {x:+unit/2,y:+unit/2}
   noSmooth();
 }
+
 function windowResized() {
   resizeCanvas(windowWidth,windowHeight);
-  old_unit = unit
-  unit = (windowWidth+windowHeight)/2 //unit is the average of the height and width of the screen, use it for all scaling and co-ordinates.
-  unit_offset.x = (windowWidth-unit)/2
-  unit_offset.y = (windowHeight-unit)/2
 }
 
-function diff(a,b){
+function diff(a,b){ //quickly find the difference between two values
   return Math.abs(a-b);
 }
 
@@ -73,34 +64,18 @@ function draw() {
   camera.y = (player1.y+player2.y)/2
 
   //find camera zoom
-  if (diff(player1.x,player2.x) > diff(player1.y,player2.y)){
-    camera.zoom = diff(player1.x,player2.x)
-    print("x")
-  }
-  else {
-    camera.zoom = diff(player1.y,player2.y)
-    print("y")
-  }
-  //zoom is the largest distance in x or y between players
-  camera.zoom += 0.5*unit //add some space on the sides
-  //zoom effectively replaces unit
 
-
-//note: zoom working so now jus fix render to work with it and new absolute values
 
   //render
   background(220);
-  image(level,0,0,windowWidth,windowHeight,camera.x-camera.zoom/2,camera.y-camera.zoom/2,width,height,CONTAIN)
-  //image(level,(-(camera.x-0.5*camera.zoom)),(-(camera.y-0.5*camera.zoom)),camera.zoom,camera.zoom) //-0.5 unit becuase the camera co-ords are for the middle but the image is drawn from the top left, negate because the image moves against the cameras movements
-  print(zoom)
+  image(level,0,0,windowWidth,windowHeight,camera.x-windowWidth/2,camera.y-windowHeight/2,width,height,CONTAIN)
+  
   if (player1.y < player2.y){
-    circle(player1.x,player1.y)
-
-    circle(player1.x-camera.x+unit_offset.x,player1.y-camera.y+unit_offset.y,unit/12)
-    circle(player2.x-camera.x+unit_offset.x,player2.y-camera.y+unit_offset.y,unit/12)
+    circle(player1.x-camera.x+windowWidth/2,player1.y-camera.y+windowHeight/2,10)
+    circle(player2.x-camera.x+windowWidth/2,player2.y-camera.y+windowHeight/2,10)
   }
   else{
-    
-
+    circle(player2.x-camera.x+windowWidth/2,player2.y-camera.y+windowHeight/2,10)
+    circle(player1.x-camera.x+windowWidth/2,player1.y-camera.y+windowHeight/2,10)
   }
 }
